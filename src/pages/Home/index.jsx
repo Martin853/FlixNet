9ba38c0861708628e02c72521732552bad719d9d
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-08-02 16:10:34 +0200

    Created popular movies section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { HeroSection } from "./HeroSection";
 import { UpcomingMovies } from "./UpcomingMovies";
+import { PopularMovies } from "./PopularMovies";
 
 export const Home = () => {
   return (
     <div className='w-full h-full flex flex-col gap-3'>
       <HeroSection />
       <UpcomingMovies />
+      <PopularMovies />
     </div>
   );
 };
