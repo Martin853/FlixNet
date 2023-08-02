71644cad2723ce82f1b05e06f842fda22123c367
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-08-02 16:13:52 +0200

    Created now playing section

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { HeroSection } from "./HeroSection";
 import { UpcomingMovies } from "./UpcomingMovies";
 import { PopularMovies } from "./PopularMovies";
+import { NowPlaying } from "./NowPlaying";
 
 export const Home = () => {
   return (
     <div className='w-full h-full flex flex-col gap-3'>
       <HeroSection />
       <UpcomingMovies />
+      <NowPlaying />
       <PopularMovies />
     </div>
   );
